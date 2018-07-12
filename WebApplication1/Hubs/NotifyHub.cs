b1fa1e7fb914ea99f5fda061ca6bbf943b1aea6b
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2018-07-12 02:19:41 -0500

    Example RT

---
+++ WebApplication1/Hubs/NotifyHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication1
+{
+    public class NotifyHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Clients.All.NotifyPush(name, message);
+        }
+    }
+}
